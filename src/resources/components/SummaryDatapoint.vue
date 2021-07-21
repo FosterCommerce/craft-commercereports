63120bea9f4f16e57020a68c7a2dddb145d1c2af
Chris <chris@clowerweb.com>
2021-07-21 09:17:55 -0400

    Reformat files

--- src/resources/components/SummaryDatapoint.vue
+++ src/resources/components/SummaryDatapoint.vue
@@ -1,68 +1,71 @@
 <script>
-	import styles from '../css/common.css';
+import styles from '../css/common.css';
 
-	export default {
-		name: 'summary-datapoint',
-		props: {
-			data: {
-			  type: Object,
-        default: () => {},
+export default {
+  name: 'summary-datapoint',
+  props: {
+    data: {
+      type: Object,
+      default: () => {
       },
-      number: {
-        type: Number,
-      },
-			format: {
-				type: String,
-				default: 'percent',
-			},
-			upDown: {
-				type: Boolean,
-				default: false,
-			},
-			placeholderWidth: {
-				type: Number,
-				default: 90,
-			},
-		},
-		methods: {
-			getTrendPhrase() {
-				if (this.data.percentChange > 0) {
-					return this.upDown ? 'up by' : 'increased by';
-				} else if (this.data.percentChange === 0) {
-				  return 'unchanged';
-        }
+    },
+    number: {
+      type: Number,
+    },
+    format: {
+      type: String,
+      default: 'percent',
+    },
+    upDown: {
+      type: Boolean,
+      default: false,
+    },
+    placeholderWidth: {
+      type: Number,
+      default: 90,
+    },
+  },
+  methods: {
+    getTrendPhrase() {
+      if (this.data.percentChange > 0) {
+        return this.upDown ? 'up by' : 'increased by';
+      } else if (this.data.percentChange === 0) {
+        return 'unchanged';
+      }
 
-				return this.upDown ? 'down by' : 'decreased by';
-			},
-			getFormattedChangeValue() {
-			  let text = this.format === 'percent' ? `${this.getTrendPhrase()} ` : '';
+      return this.upDown ? 'down by' : 'decreased by';
+    },
+    getFormattedChangeValue() {
+      let text = this.format === 'percent' ? `${this.getTrendPhrase()} ` : '';
 
-        if (this.format === 'percent' && this.data.percentChange !== 0) {
-          text += `${Math.abs(this.data.percentChange)}%`;
-        }
+      if (this.format === 'percent' && this.data.percentChange !== 0) {
+        text += `${Math.abs(this.data.percentChange)}%`;
+      }
 
-			  if (this.format === 'orders') {
-			    text += `${this.data.total} ${this.total !== 1 ? 'orders' : 'order'}`;
-			    text += ` (${this.getTrendPhrase()}${this.data.percentChange !== 0 ? ' ' + Math.abs(this.data.percentChange) + '%' : ''})`;
-        }
+      if (this.format === 'orders') {
+        text += `${this.data.total} ${this.total !== 1 ? 'orders' : 'order'}`;
+        text += ` (${this.getTrendPhrase()}${this.data.percentChange !== 0 ?
+          ' ' + Math.abs(this.data.percentChange) + '%' :
+          ''})`;
+      }
 
-				return text;
-			},
-		},
-	};
+      return text;
+    },
+  },
+};
 </script>
 
 <template>
 	<span
     v-if="data"
-		:class="{
+    :class="{
 			'placeholder-summary-text': data.percentChange === undefined || data.percentChange === NaN,
 			'commerce-insights-up': data.percentChange > 0,
 			'commerce-insights-down': data.percentChange < 0,
 			'commerce-insights-unchanged': data.percentChange === 0,
 		}"
-		class="commerce-insights-summary-datapoint"
-	>
+    class="commerce-insights-summary-datapoint"
+  >
 		{{ getFormattedChangeValue() }}
 	</span>
 
@@ -82,15 +85,15 @@
 </template>
 
 <style>
-	.commerce-insights-summary-datapoint {
-		transition: width 0.5s linear;
-		display: inline-block;
-	}
+.commerce-insights-summary-datapoint {
+  transition: width 0.5s linear;
+  display: inline-block;
+}
 
-	.placeholder-summary-text {
-		display: inline-block;
-		background-color: #f1f5f8;
-		color: #f1f5f8;
-		border-radius: 3px;
-	}
+.placeholder-summary-text {
+  display: inline-block;
+  background-color: #f1f5f8;
+  color: #f1f5f8;
+  border-radius: 3px;
+}
 </style>
--- src/resources/index.js
+++ src/resources/index.js
@@ -16,230 +16,226 @@ import moment from 'moment';
 import qs from 'qs';
 
 const app = new Vue({
-    el: '#main',
-    delimiters: ['${', '}'],
-    components: {
-        'bar-chart-panel': BarChartPanel,
-        'combined-search': CombinedSearch,
-        'chart': Chart,
-        'date-range-picker': DateRangePicker,
-        'donut-panel': DonutPanel,
-        'line-chart-panel': LineChartPanel,
-        'mini-sparkline-panel': MiniSparklinePanel,
-        'pane': Pane,
-        'sparkline-panel': SparklinePanel,
-        'summary-datapoint': SummaryDatapoint,
-        'tab': Tab,
-        'tabbed-panes': TabbedPanes
+  el: '#main',
+  delimiters: ['${', '}'],
+  components: {
+    'bar-chart-panel': BarChartPanel,
+    'combined-search': CombinedSearch,
+    'chart': Chart,
+    'date-range-picker': DateRangePicker,
+    'donut-panel': DonutPanel,
+    'line-chart-panel': LineChartPanel,
+    'mini-sparkline-panel': MiniSparklinePanel,
+    'pane': Pane,
+    'sparkline-panel': SparklinePanel,
+    'summary-datapoint': SummaryDatapoint,
+    'tab': Tab,
+    'tabbed-panes': TabbedPanes,
+  },
+  data() {
+    return {
+      dateRange: {
+        start: moment().subtract(7, 'days').format('YYYY-MM-DD 00:00:00'),
+        end: moment().format('YYYY-MM-DD 23:59:59'),
+      },
+      stats: {
+        'orders': {
+          'summary': {},
+          'topLocations': [],
+          'totalOrders': {},
+          'averageValue': {},
+          'averageQuantity': {},
+          'totalCustomers': {},
+          'newCustomers': {},
+          'returningCustomers': {},
+        },
+        'products': {
+          'summary': {},
+          'mostPurchased': {},
+          'mostProfitable': [],
+        },
+        'customers': {
+          'summary': {},
+        },
+      },
+      orders: [],
+      products: [],
+      sales: [],
+      customers: [],
+    };
+  },
+  methods: {
+    getUrlParam(param) {
+      const url = window.location.href;
+      const name = param.replace(/[\[\]]/g, '\\$&');
+      const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
+      const res = regex.exec(url);
+
+      if (!res) return null;
+      if (!res[2]) return '';
+      return !res ? null : !res[2] ? '' : res[2];
     },
-    data() {
-        return {
-            dateRange: {
-                start: moment().subtract(7, 'days').format('YYYY-MM-DD 00:00:00'),
-                end: moment().format('YYYY-MM-DD 23:59:59'),
-            },
-            stats: {
-                "orders": {
-                    "summary": {},
-                    "topLocations": [],
-                    "totalOrders": {},
-                    "averageValue": {},
-                    "averageQuantity": {},
-                    "totalCustomers": {},
-                    "newCustomers": {},
-                    "returningCustomers": {}
-                },
-                "products": {
-                    "summary": {},
-                    "mostPurchased": {},
-                    "mostProfitable": []
-                },
-                "customers": {
-                    "summary": {}
-                }
-            },
-            orders: [],
-            products: [],
-            sales: [],
-            customers: []
+    handleDateChange(dates) {
+      const start = moment(dates.start).format('YYYY-MM-DD 00:00:00');
+      const end = moment(dates.end).format('YYYY-MM-DD 23:59:59');
+      const loaders = document.getElementsByClassName('commerce-insights-ajax-loader');
+
+      if (loaders.length) {
+        for (let loader of loaders) {
+          loader.classList.remove('hidden');
         }
+      }
+
+      this.dateRange.start = start;
+      this.dateRange.end = end;
+
+      this.refreshAllData();
     },
-    methods: {
-        getUrlParam(param) {
-            const url = window.location.href;
-            const name = param.replace(/[\[\]]/g, '\\$&');
-            const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)');
-            const res = regex.exec(url);
+    refreshAllData() {
+      const page_url = window.location.href;
+      let page = page_url.substr(page_url.lastIndexOf('/') + 1);
+      const idx = page.indexOf('?');
 
-            if (!res) return null;
-            if (!res[2]) return '';
-            return !res ? null : !res[2] ? '' : res[2];
-        },
-        handleDateChange(dates) {
-            const start = moment(dates.start).format('YYYY-MM-DD 00:00:00');
-            const end = moment(dates.end).format('YYYY-MM-DD 23:59:59');
-            const loaders = document.getElementsByClassName('commerce-insights-ajax-loader');
+      page = page.substring(0, idx !== -1 ? idx : page.length);
 
-            if(loaders.length) {
-                for(let loader of loaders) {
-                    loader.classList.remove('hidden');
-                }
-            }
+      this.fetchStats();
 
-            this.dateRange.start = start;
-            this.dateRange.end = end;
+      switch (page) {
+        case 'orders':
+          this.fetchOrders();
+          break;
+        case 'sales':
+          this.fetchSales();
+          break;
+        case 'products':
+          this.fetchProducts();
+          break;
+        case 'customers':
+          this.fetchCustomers();
+          break;
+        default:
+          if (isNaN(page)) {
+            this.fetchOrders();
+            this.fetchSales();
+            this.fetchProducts();
+            this.fetchCustomers();
+          } else {
+            this.fetchItemOrders(page);
+          }
+      }
+    },
+    fetchStats() {
+      const self = this;
+      const data = {
+        range_start: self.dateRange.start,
+        range_end: self.dateRange.end,
+      };
 
-            this.refreshAllData();
-        },
-        refreshAllData() {
-            const page_url = window.location.href;
-            let page = page_url.substr(page_url.lastIndexOf('/') + 1);
-            const idx = page.indexOf('?');
+      data[Craft.csrfTokenName] = Craft.csrfTokenValue;
 
-            page = page.substring(0, idx !== -1 ? idx : page.length);
+      axios.post('/actions/commerceinsights/vue/get-stats', qs.stringify(data)).then(response => {
+        self.stats = response.data;
+      }).catch(error => {
+        console.log(error);
+      });
+    },
+    fetchOrders() {
+      const self = this;
+      const data = {
+        range_start: self.dateRange.start,
+        range_end: self.dateRange.end,
+      };
+      let url = '/actions/commerceinsights/vue/get-orders';
 
-            this.fetchStats();
+      data[Craft.csrfTokenName] = Craft.csrfTokenValue;
 
-            switch(page) {
-                case 'orders':
-                    this.fetchOrders();
-                break;
-                case 'sales':
-                    this.fetchSales();
-                break;
-                case 'products':
-                    this.fetchProducts();
-                break;
-                case 'customers':
-                    this.fetchCustomers();
-                break;
-                default:
-                    if(isNaN(page)) {
-                        this.fetchOrders();
-                        this.fetchSales();
-                        this.fetchProducts();
-                        this.fetchCustomers();
-                    } else {
-                        this.fetchItemOrders(page);
-                    }
-            }
-        },
-        fetchStats() {
-            const self = this;
-            const data = {
-                range_start: self.dateRange.start,
-                range_end: self.dateRange.end
-            };
+      axios.post(url, qs.stringify(data)).then(response => {
+        self.orders = response.data;
+      }).catch(error => {
+        console.log(error);
+      });
+    },
+    fetchItemOrders(item) {
+      const self = this;
+      const variant = self.getUrlParam('variant');
+      const variant_type = self.getUrlParam('variant_type');
+      const color = self.getUrlParam('color');
+      const start = self.getUrlParam('startDate');
+      const end = self.getUrlParam('endDate');
+      const data = {
+        range_start: self.dateRange.start,
+        range_end: self.dateRange.end,
+      };
+      let url = '/admin/commerceinsights/orders/product/' + item;
 
-            data[Craft.csrfTokenName] = Craft.csrfTokenValue;
+      if (variant || variant_type || color || start || end) url += '?';
 
-            axios.post('/actions/commerceinsights/vue/get-stats', qs.stringify(data))
-            .then(response => {
-                self.stats = response.data;
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
-        fetchOrders() {
-            const self = this;
-            const data = {
-                range_start: self.dateRange.start,
-                range_end: self.dateRange.end
-            };
-            let url = '/actions/commerceinsights/vue/get-orders';
+      if (start) {
+        url += `startDate=${start}`;
+      }
 
-            data[Craft.csrfTokenName] = Craft.csrfTokenValue;
+      if (end) {
+        url += start ? `&endDate=${end}` : `endDate=${end}`;
+      }
 
-            axios.post(url, qs.stringify(data))
-            .then(response => {
-                self.orders = response.data;
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
-        fetchItemOrders(item) {
-            const self = this;
-            const variant = self.getUrlParam('variant');
-            const variant_type = self.getUrlParam('variant_type');
-            const color = self.getUrlParam('color');
-            const start = self.getUrlParam('startDate');
-            const end = self.getUrlParam('endDate');
-            const data = {
-                range_start: self.dateRange.start,
-                range_end: self.dateRange.end
-            };
-            let url = '/admin/commerceinsights/orders/product/' + item;
+      if (variant) {
+        url += start || end ? `&variant=${variant}` : `variant=${variant}`;
+      }
 
-            if(variant || variant_type || color || start || end) url += '?';
+      if (variant_type) {
+        url += start || end || variant ?
+          `&variant_type=${variant_type}` :
+          `variant_type=${variant_type}`;
+      }
 
-            if(start) {
-                url += `startDate=${start}`;
-            }
+      if (color) {
+        url += start || end || variant || variant_type ? `&color=${color}` : `color=${color}`;
+      }
 
-            if(end) {
-                url += start ? `&endDate=${end}` : `endDate=${end}`;
-            }
+      data[Craft.csrfTokenName] = Craft.csrfTokenValue;
+      data['render'] = false;
+      data['id'] = item;
 
-            if(variant) {
-                url += start || end ? `&variant=${variant}` : `variant=${variant}`;
-            }
+      axios.post(url, qs.stringify(data)).then(response => {
+        console.log(response);
+        self.orders = response.data;
+      }).catch(error => {
+        console.log(error);
+      });
+    },
+    fetchProducts() {
+      const self = this;
+      const data = {
+        range_start: self.dateRange.start,
+        range_end: self.dateRange.end,
+      };
 
-            if(variant_type) {
-                url += start || end || variant ? `&variant_type=${variant_type}` : `variant_type=${variant_type}`;
-            }
+      data[Craft.csrfTokenName] = Craft.csrfTokenValue;
 
-            if(color) {
-                url += start || end || variant || variant_type ? `&color=${color}` : `color=${color}`;
-            }
+      axios.post('/actions/commerceinsights/vue/get-products', qs.stringify(data)).
+        then(response => {
+          self.products = response.data;
+        }).
+        catch(error => {
+          console.log(error);
+        });
+    },
+    fetchCustomers() {
+      const self = this;
+      const data = {
+        range_start: self.dateRange.start,
+        range_end: self.dateRange.end,
+      };
 
-            data[Craft.csrfTokenName] = Craft.csrfTokenValue;
-            data['render'] = false;
-            data['id'] = item;
+      data[Craft.csrfTokenName] = Craft.csrfTokenValue;
 
-            axios.post(url, qs.stringify(data))
-            .then(response => {
-                console.log(response);
-                self.orders = response.data;
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
-        fetchProducts() {
-            const self = this;
-            const data = {
-                range_start: self.dateRange.start,
-                range_end: self.dateRange.end
-            };
-
-            data[Craft.csrfTokenName] = Craft.csrfTokenValue;
-
-            axios.post('/actions/commerceinsights/vue/get-products', qs.stringify(data))
-            .then(response => {
-                self.products = response.data;
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        },
-        fetchCustomers() {
-            const self = this;
-            const data = {
-                range_start: self.dateRange.start,
-                range_end: self.dateRange.end
-            };
-
-            data[Craft.csrfTokenName] = Craft.csrfTokenValue;
-
-            axios.post('/actions/commerceinsights/vue/get-customers', qs.stringify(data))
-            .then(response => {
-                self.customers = response.data;
-            })
-            .catch(error => {
-                console.log(error);
-            });
-        }
-    }
+      axios.post('/actions/commerceinsights/vue/get-customers', qs.stringify(data)).
+        then(response => {
+          self.customers = response.data;
+        }).
+        catch(error => {
+          console.log(error);
+        });
+    },
+  },
 });
