fecaa5abca74c70edd1335b00c87d8d50068d842
Chris <chris@clowerweb.com>
2021-10-15 13:20:48 -0400

    Refactor Order model

--- src/models/OrderModel.php
+++ src/models/OrderModel.php
@@ -20,42 +20,56 @@ class OrderModel extends Model
     /**
      * Formats the data for orders.
      *
+     * @param array $orders - The orders to format
+     *
      * @return array
      */
     public static function fromOrders(array $orders): array {
-        $currentPeriod = $orders['currentPeriod'] ?? [];
-        $result = [];
+        $currentPeriod = $orders['currentPeriod'] ?? $orders;
+        $results = [];
 
         foreach ($currentPeriod as $idx => $order) {
-            $currency   = $order->currency;
-            $line_items = $order->lineItems;
-            $result[]   = [
-                'orderId'       => $order->id,
-                'reference'     => $order->friendlyOrderNumber ?? $order->reference,
-                'date'          => $order->dateOrdered->format('m/d/Y g:ia'),
-                'fullDate'      => $order->dateOrdered->format('l, F j, Y, g:ia'),
-                'timeStamp'     => $order->dateOrdered->getTimestamp(),
-                'status'        => $order->orderStatus->name,
-                'color'         => $order->orderStatus->color,
-                'base'          => Helpers::convertCurrency(($order->total - $order->adjustmentSubtotal), $currency),
-                'merchTotal'    => Helpers::convertCurrency(($order->total - $order->totalTax - $order->totalShippingCost - $order->totalDiscount), $currency),
-                'tax'           => Helpers::convertCurrency($order->totalTax, $currency),
-                'shipping'      => Helpers::convertCurrency($order->totalShippingCost, $currency),
-                'discount'      => Helpers::convertCurrency($order->totalDiscount, $currency),
-                'amountPaid'    => Helpers::convertCurrency($order->totalPaid, $currency),
-                'paymentStatus' => ucwords($order->paidStatus),
-                'paidColor'     => $order->paidStatus === 'paid' ? 'green' : 'red',
-                'email'         => $order->customer->email,
-                'billingName'   => ($order->billingAddress->firstName ?? ' ') . ' ' . ($order->billingAddress->lastName ?? ' '),
-                'shippingName'  => ($order->shippingAddress->firstName ?? ' ') . ' ' . ($order->shippingAddress->lastName ?? ' '),
-                'numItems'      => 0
-            ];
+            $lineItems = $order->lineItems;
+            $results[] = self::fromOrder($order);
 
-            foreach ($line_items as $item) {
-                $result[$idx]['numItems'] += $item->qty;
+            foreach ($lineItems as $item) {
+                $results[$idx]['numItems'] += $item->qty;
             }
         }
 
-        return $result;
+        return $results;
+    }
+
+    /**
+     * Formats the data for a single order.
+     *
+     * @param object $order - The order to format
+     *
+     * @return array
+     */
+    public static function fromOrder(object $order): array {
+        $currency = $order->currency;
+
+        return [
+            'orderId'       => $order->id,
+            'reference'     => $order->friendlyOrderNumber ?? $order->reference,
+            'date'          => $order->dateOrdered->format('m/d/Y g:ia'),
+            'fullDate'      => $order->dateOrdered->format('l, F j, Y, g:ia'),
+            'timeStamp'     => $order->dateOrdered->getTimestamp(),
+            'status'        => $order->orderStatus->name,
+            'color'         => $order->orderStatus->color,
+            'base'          => Helpers::convertCurrency(($order->total - $order->adjustmentSubtotal), $currency),
+            'merchTotal'    => Helpers::convertCurrency(($order->total - $order->totalTax - $order->totalShippingCost - $order->totalDiscount), $currency),
+            'tax'           => Helpers::convertCurrency($order->totalTax, $currency),
+            'shipping'      => Helpers::convertCurrency($order->totalShippingCost, $currency),
+            'discount'      => Helpers::convertCurrency($order->totalDiscount, $currency),
+            'amountPaid'    => Helpers::convertCurrency($order->totalPaid, $currency),
+            'paymentStatus' => ucwords($order->paidStatus),
+            'paidColor'     => $order->paidStatus === 'paid' ? 'green' : 'red',
+            'email'         => $order->customer->email,
+            'billingName'   => ($order->billingAddress->firstName ?? ' ') . ' ' . ($order->billingAddress->lastName ?? ' '),
+            'shippingName'  => ($order->shippingAddress->firstName ?? ' ') . ' ' . ($order->shippingAddress->lastName ?? ' '),
+            'numItems'      => 0
+        ];
     }
 }
