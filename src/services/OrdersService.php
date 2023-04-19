c9699e7e41772cbd6dab4892a351a7ea0c35a7a0
Pete Eveleigh <pete@fantasticmachine.co.uk>
2023-04-19 22:15:55 +0100

    replace initial element query when fetching orders

--- src/services/OrdersService.php
+++ src/services/OrdersService.php
@@ -20,6 +20,7 @@ use Craft;
 use craft\base\Component;
 use craft\commerce\elements\Order;
 use craft\commerce\elements\Variant;
+use craft\helpers\Db;
 
 class OrdersService extends Component
 {
@@ -59,9 +60,52 @@ class OrdersService extends Component
     public function fetchOrders(array $opts = []): array {
         $productId    = $opts['productId'] ?? null;
         $withPrevious = $opts['withPrevious'] ?? true;
-        $orders       = Order::find()->distinct()->orderBy('dateOrdered desc');
         $result       = [];
-
+        
+        //$orders       = Order::find()->distinct()->orderBy('dateOrdered desc');
+        
+        $orders = (new \craft\db\Query())
+            ->select([
+                'o.id',
+                'o.reference',
+                'o.dateOrdered',
+                'os.name as `orderStatus`',
+                'o.itemTotal',
+                'o.totalTax',
+                'o.totalTaxIncluded',
+                'o.totalDiscount',
+                'o.totalShippingCost',
+                'o.totalPaid',
+                '(select sum(l.qty) from commerce_lineitems l where l.orderId = o.id) as `totalItemsSold`',
+                'o.paidStatus'
+            ])
+            ->from('commerce_orders o')
+            ->join('LEFT JOIN', 'commerce_orderstatuses os', 'os.id = o.orderStatusId')
+            ->orderBy('o.dateOrdered desc');
+            //->all();
+        
+        
+        /*$query = Db::db()->createCommand("select 
+        o.id as 'id',
+        o.reference as 'Order #',
+        o.dateOrdered as 'Date Ordered',
+        os.name as 'Status',
+        o.itemTotal as 'Merchandise Total',
+        o.totalTax as 'Tax',
+        o.totalTaxIncluded as 'Total Tax Included',
+        o.totalDiscount as 'Discount',
+        o.totalShippingCost as 'Shipping',
+        o.totalPaid as 'Total Paid',
+        (select sum(l.qty) from `commerce_lineitems` l where l.orderId = o.id)  as 'Total Items Sold',
+        o.paidStatus as 'Payment Status'
+        from commerce_orders o
+        left join commerce_orderstatuses os on os.id = o.orderStatusId
+        #left join commerce_lineitems l on l.orderId = o.id
+        order by o.dateOrdered desc");
+        
+        $orders = $query->queryAll();
+        
+        
         if ($productId) {
             $product = Variant::find()->id($productId)->one();
             $orders->hasPurchasables([$product]);
@@ -87,7 +131,30 @@ class OrdersService extends Component
                 'currentPeriod'  => $result
             ];
         }
+        
+        */
+        
+        if ($this->keyword) {
+            $orders->search($this->keyword);
+        }
+        
+        if ($this->orderType) {
+            $orders->where("o.orderStatus = :orderStatus", [':orderStatus' => strtolower($this->paymentType)]);
+        }
+        
+        if ($this->paymentType) {
+            $orders->where("o.orderStatus = :orderStatus", [':orderStatus' => strtolower($this->orderType)]);
+        }
+        
+        $orders->where("o.dateOrdered >= :startDate and o.dateOrdered < :endDate", [':startDate' => $this->dates['originalStart'], ':endDate' => $this->dates['originalEnd']]);
+        
+        
+        $result['currentPeriod'] = $orders->all();
 
+        if($withPrevious) {
+            $result['previousPeriod'] = $orders->where("o.dateOrdered >= :startDate and o.dateOrdered < :endDate", [':startDate' => $this->dates['previousStart'], ':endDate' => $this->dates['originalStart']])->all();
+        }
+        
         return $result;
     }
 
